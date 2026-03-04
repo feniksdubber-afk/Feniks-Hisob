c1b6f6622e66c9464563b494aa3c14e3a3aef091
feniksdubber-afk <feniksdubber@gmail.com>
2026-03-04 06:35:34 +0500

    Update static buttons in config.py
    Added new options for employee status and personal status to static buttons.

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
 import os
 
-# Telegram Bot Token va Admin ID
 TOKEN = '6844735110:AAE1y58TDn1Kah9SewVx_IFTHBXNZOupJ4w'
 ADMIN_ID = 6761276533
 CHANNEL_ID = -1003634886616 
 
-# Baza fayllari (CSV)
 DB_FILE = "feniks_v12.csv"
 PROJECTS_FILE = "projects_v12.csv"
 MENU_FILE = "custom_menu_v12.csv"
 TASKS_FILE = "tasks_v12.csv"
 TEXTS_FILE = "texts_v12.csv"
 
-# Barcha xodimlar ko'radigan qismlar
 STATIC_BUTTONS = [
     "📊 Moliya", "💸 Ishchilarga pul tashlash", "🎬 Loyihalar", 
     "👤 Xodim Qo'shish", "👤 Xodimni O'chirish", "📣 E'lon Yuborish", 
@@ -20,5 +17,5 @@ STATIC_BUTTONS = [
     "🛠 Menu Builder", "🔤 Tizim Matnlari", "🎙 Ovoz/Material topshirish", 
     "💰 Mening Hisobim", "📋 Faol Vazifalar", "🔐 Parolni o'zgartirish", 
     "🎧 Tayyor Material Yuborish", "✍️ Tayyor Elonni yuborish", 
-    "💬 Adminga Savol/Xabar" # YANGI QO'SHILGAN FUNKSIYA
+    "💬 Adminga Savol/Xabar", "📈 Ishchilar Holati", "📈 Mening Holatim"
 ]
